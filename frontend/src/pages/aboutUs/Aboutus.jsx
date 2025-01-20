33ca424fb02ca97666459915d54c9555ecf37dbb
shreejanmdr <shreejan0497@gmail.com>
2025-01-21 02:27:26 +0545

    edited

--- frontend/src/pages/aboutUs/Aboutus.jsx
+++ frontend/src/pages/aboutUs/Aboutus.jsx
@@ -1,33 +1,3 @@
-// import React from 'react'
-// import './Aboutus.css'
-
-// const Aboutus = () => {
-//   return (
-//     <>
-//     <div className='pt-5'>
-//         <div className='heading'>
-//             <h1>About Us</h1>
-//             <p>A real estate management system, to make you find your desired property easier.</p>
-//         </div>
-//         <div className='container'>
-//             <section className='about'>
-//                 <div className='about-image'>
-//                     <img src='/assets/images/register.png'></img>
-//                 </div>
-//                 <div className='about-content'>
-//                     <h2 style={{color: '#083775'}}>Welcome to Estate Ease!</h2>
-//                     <p>At our real estate management company, we are committed to offering our clients a diverse range of the latest and most innovative property solutions. Our journey has been extensive, giving us the expertise to provide you with premium quality services that are both efficient and cost-effective.</p>
-//                 </div>
-//             </section>
-//         </div>
-//     </div>
-//     </>
-//   )
-// }
-
-// export default Aboutus
-
-
 import React from 'react';
 import { Box, Container, Typography, Grid, Card, CardContent, CardMedia } from '@mui/material';
 import './Aboutus.css';
